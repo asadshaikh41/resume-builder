c73d566e097a5afbbfb60952452722d6766c51f1
asadshaikh41 <87890728+asadshaikh41@users.noreply.github.com>
2025-02-20 16:51:19 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://resume-builder-server-ecru.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
