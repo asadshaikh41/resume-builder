e3d65e151c407b130d7454b183b655f7daa115ee
asadshaikh41 <87890728+asadshaikh41@users.noreply.github.com>
2025-02-20 16:50:34 +0530

    Update Register.jsx

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://resume-builder-server-ecru.vercel.app/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
